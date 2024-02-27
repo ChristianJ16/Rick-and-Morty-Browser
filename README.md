ced67d12b77b0746da3c4e1bd810be212f2db06e
christianj16 <christianjansen16@gmail.com>
2024-02-26 21:57:08 -0800

    random image updated

---
+++ README.md
@@ -0,0 +1,7 @@
+I am building a website that allows you to search up an episode number for Rick and Morty and it will tell you some details for the episode.
+
+I used the Fetch API to pull data from the Rick and Morty API that I'm using for this project. 
+
+I created a random image button that will show you random pictures of Rick and Morty. I used an addEventListener for this that repsonds to a 'click'. I created an array of the images of Rick and Morty and then created a randomizer using Math.random to choose an image at random when the button is clicked.
+
+The approach I took to get this project completed was I started off by making all my .html, .js, and .css we're all linked properly. My next step was to get the website to use the API data successfully. Then I chose which keys I wanted to be displayed and added them in the .js. Then I had to figure out to make the random image button. Once the core functionalities of the website were there is when I decided to focus a bit more on the CSS.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-  document.querySelector('form').addEventListener('submit',(e) => {
-    e.preventDefault()
+  document.querySelector('form').addEventListener('submit',(e) => {  // looks for the first form element in the html and looks for the submit event when the the form is submitted it called the arrowfunction
+    e.preventDefault() // prevents a page reload when the form is submitted
     const userInput = document.querySelector('input[type="text"]').value
 
     fetch('https://rickandmortyapi.com/api/episode/' + userInput).then(
@@ -26,20 +26,22 @@
   })
 
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function(){ // listens for when the HTML document has finished loading
   const button = document.getElementById("randomImageButton")
   const image = document.getElementById("randomImage")
-  button.addEventListener("click", function() {
-    const images = [
-      "rick-and-morty-30973.png",
-      "rick-and-morty-30991.png",
-      "rick-and-morty-30998.png",
-      "rick-and-morty-31012.png",
-      "rick-and-morty-31015.png",
-      "rick-and-morty-31043.png",
+  button.addEventListener("click", function() { 
+    const images = [ // array of the images
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/list-rick-and-morty-episodes-wikipedia-24.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-moon-mod-download-35.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-mind-bending-season-mysteries-quidd-6.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-rick-face-kushmastafresh-deviantart-19.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/miami-rick-pocket-mortys-rick-and-morty-wiki-fandom-16.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-rick-morty-projectacademy-medium-17.png",
+    "https://www.freepnglogos.com/uploads/rick-and-morty/rick-and-morty-background-rick-morty-wallpaper-speedpaint-youtube-2.jpg",
+
     ]
     const randomIndex = Math.floor(Math.random() * images.length)
-    const randomImage = images[randomIndex]
-    image.src = "/Users/christianjansen/Desktop/mini-project/imgs/" + randomImage
+    const randomImage = images[randomIndex] // selects a random image URL from the images array using the random index generated above this line
+    image.src = randomImage // source of the images
   })
 })
